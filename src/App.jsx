146c23f731f3bdc8e200091c4ec4bb9676964a91
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-08 18:12:22 +0530

    transcript part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+const WHISPER_ENDPOINT = " https://api.openai.com/v1/audio/transcriptions";
+
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [transcript, setTranscript] = useState("");
+  const [summary, setSummary] = useState("");
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleGenerateSummary = async () => {
+    if (!selectedFile) {
+      alert("Please select a file to generate summary");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", selectedFile);
+    formData.append("model", "whisper-1");
+
+    try {
+      const response = await fetch(WHISPER_ENDPOINT, {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Bearer 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",
+        },
+        body: formData,
+      });
+      const result = await response.json();
+      setTranscript(result.text);
+      setSummary("Summary shows here");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="App">
       <div className="upload-div">
@@ -9,9 +46,18 @@ function App() {
           <h1>Upload</h1>
         </div>
         <div className="upload-btns content">
-          <button className="upload">Select audio file</button>
-          <p>No file selected</p>
-          <button className="upload">Upload</button>
+          <label className="upload">
+            Select audio file
+            <input
+              type="file"
+              style={{ display: "none" }}
+              onChange={handleFileSelect}
+            />
+          </label>
+          <p>{selectedFile ? selectedFile.name : "No file selected"}</p>
+          <button className="upload-2" onClick={handleGenerateSummary}>
+            Generate Transcript
+          </button>
         </div>
       </div>
       <div className="text-div">
@@ -20,7 +66,7 @@ function App() {
             <h3>Transcript</h3>
           </div>
           <div className="transcript-text content">
-            <p>hello hello</p>
+            <p>{transcript}</p>
           </div>
         </div>
         <div className="summary-div">
@@ -28,7 +74,7 @@ function App() {
             <h3>Summary</h3>
           </div>
           <div className="summary-text content">
-            <p>hello hello</p>
+            <p>{summary}</p>
           </div>
         </div>
       </div>
