22d67395387e98696bbcbf7c53b01e0bbe87850f
koushikjoshi <39240894+koushikjoshi@users.noreply.github.com>
2023-03-09 01:04:14 +0530

    major UI update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import { MdContentCopy } from "react-icons/md";
 
 const WHISPER_ENDPOINT = "https://api.openai.com/v1/audio/transcriptions";
 const HELLO = "hello";
@@ -8,10 +9,6 @@ function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [transcript, setTranscript] = useState("");
   const [summary, setSummary] = useState("");
-
-  const handleFileSelect = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
 
   const handleGenerateSummary = async () => {
     if (!selectedFile) {
@@ -38,40 +35,54 @@ function App() {
       console.error("Error:", error);
     }
   };
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+    handleGenerateSummary();
+  };
 
   return (
     <div className="App">
       <div className="upload-div">
-        <div className="heading">
-          <h1>Upload</h1>
+        <div>
+          <img className="head-img" src={require("./logo.png")} alt="" />
         </div>
-        <div className="upload-btns content">
-          <label className="upload">
-            Select audio file
-            <input
-              type="file"
-              style={{ display: "none" }}
-              onChange={handleFileSelect}
-            />
-          </label>
-          <p>{selectedFile ? selectedFile.name : "No file selected"}</p>
-          <button className="upload-2" onClick={handleGenerateSummary}>
-            Generate Transcript
-          </button>
+        <div className="heading-1">
+          <h1>AutoBrainstorm</h1>
+        </div>
+        <div className="upload-content">
+          <div className="upload-text">
+            <p>Record your random thoughts, and get a point-by-point summary</p>
+          </div>
+          <div className="upload-logic">
+            <div className="record-btn">Start recording</div>
+            <div className="upload">
+              <label>
+                OR <u style={{ cursor: "pointer" }}>Upload Audio</u>
+                <input
+                  type="file"
+                  style={{ display: "none" }}
+                  onChange={handleFileSelect}
+                />
+              </label>
+            </div>
+          </div>
         </div>
       </div>
       <div className="text-div">
         <div className="transcript-div">
           <div className="heading">
-            <h3>Transcript</h3>
+            <p>Transcript</p>
           </div>
           <div className="transcript-text content">
-            <p>{transcript}</p>
+            <div className="final-transcript">
+              <p>{transcript}</p>
+            </div>
           </div>
         </div>
         <div className="summary-div">
-          <div className="heading">
-            <h3>Summary</h3>
+          <div className="heading summary-heading">
+            <p className="summary-p">Summary</p>
+            <MdContentCopy className="copy-icon" size={15} />
           </div>
           <div className="summary-text content">
             <p>{summary}</p>
